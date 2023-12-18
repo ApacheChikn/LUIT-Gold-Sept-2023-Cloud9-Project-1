c59bf692efa29c81265486fdc115a3d0f745ce90
ApacheChikn <shawnfeltmate.gmail.com>
2023-12-18 16:51:37 +0000

    Repetition

--- repetition.py
+++ repetition.py
@@ -4,19 +4,18 @@ number = random.randint(0,1000)
 counter = 0
 
 while number != 7:
-  counter = counter + 1 # counter += 1
-  number = random.randint(0,1000)
-
-  if counter > 1100:
-    break
-
+    counter = counter + 1 # counter += 1
+    number = random.randint(0,1000)
+    
+    if counter > 1100:
+        break
+    
 print(counter, number)
 
-
 for i in range(10):
-  print(i)
-
+    print(i*2)
+    
 letters = ['A', 'B', 'C', 'D']
 
 for letter in letters:
-  print(letter.lower())
+    print(letter.lower())
